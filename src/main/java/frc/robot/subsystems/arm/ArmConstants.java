0f3705ddbf3e8398b257b9ab56918e4992850629
rook3212 <omrilevin0804@gmail.com>
2025-01-12 19:48:13 +0200

    now fixed constants order?

--- src/main/java/frc/robot/subsystems/arm/ArmConstants.java
+++ src/main/java/frc/robot/subsystems/arm/ArmConstants.java
@@ -14,10 +14,9 @@ import edu.wpi.first.math.controller.PIDController;
 public class ArmConstants {
     private static final int MOTOR_ID = 5;
     static final TalonFX MOTOR = new TalonFX(MOTOR_ID);
-
     private static final int ENCODER_ID = 34;
     private static final CANcoder ENCODER = new CANcoder(ENCODER_ID);
-
+    static final StatusSignal<Double> ANGLE_ENCODER_POSITION_SIGNAL = ENCODER.getPosition();
     private static final double
             P = 3,
             I = 0,
@@ -25,22 +24,15 @@ public class ArmConstants {
     static final PIDController PID_CONTROLLER = new PIDController(P, I, D);
 
     private static final SensorDirectionValue SENSOR_DIRECTION_VALUE = SensorDirectionValue.CounterClockwise_Positive;
-
     private static final InvertedValue INVERTED_VALUE = InvertedValue.CounterClockwise_Positive;
-
     private static final NeutralModeValue NEUTRAL_MODE_VALUE = NeutralModeValue.Brake;
-
     private static final double GEAR_RATIO = 2.5;
-
-    static final StatusSignal<Double> ANGLE_ENCODER_POSITION_SIGNAL = ENCODER.getPosition();
-
     public static final boolean FOC_ENABLED = true;
 
     static {
         configureMotor();
         configureEncoder();
     }
-
     public static void configureMotor() {
         TalonFXConfiguration config = new TalonFXConfiguration();
         config.Audio.BeepOnConfig = false;
@@ -51,7 +43,6 @@ public class ArmConstants {
         MOTOR.getConfigurator().apply(config);
         MOTOR.optimizeBusUtilization();
     }
-
     public static void configureEncoder() {
         CANcoderConfiguration config = new CANcoderConfiguration();
         config.withMagnetSensor(new MagnetSensorConfigs().withSensorDirection(SENSOR_DIRECTION_VALUE));
